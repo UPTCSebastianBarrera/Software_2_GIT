85fa8aba75f9d0e4969ed6f16ce68bdb4851f7a2
Andres F Parra <108076867+AndresfelipePa@users.noreply.github.com>
2023-09-07 16:19:27 -0500

    Update responses.py

--- responses.py
+++ responses.py
@@ -9,8 +9,13 @@ def sample_responses(input_text):
     if user_message in ("1) ¿Cuándo inician las inscripciones?"):
         return "Cordial saludo La Universidad convocará semestralmente la inscripción de aspirantes para ingresar a sus diferentes programas académicos, mediante aviso en un periódico de circulación nacional y regional, en las carteleras de la Institución y en otros medios masivos de comunicación, anunciando los requisitos exigidos y las fechas establecidas para tal efecto."
     
-    if user_message in ("2"):
-        return "Placeholder 2"
+    if user_message in ("2) ¿Qué requisitos se requieren para el proceso de transferencia interna o externa?"):
+        return "Cordial saludo."
+        "Dando respuesta a su solicitud, Los requisitos a tener en cuenta son:"
+        "- No haber transcurrido más de dos semestres, desde el último cursado."
+        "- Acreditar un promedio acumulado mínimo de 3.6 en las calificaciones del programa académico de procedencia"
+        "- Acreditar un puntaje en el Examen de Estado superior o igual al del último estudiante admitido, durante el semestre inmediatamente anterior, sin considerar el puntaje de los admitidos por condiciones especiales."
+        "Por lo tanto, debe adjuntar certificado de notas por semestre y resultado icfes para realizar verificación y estudio"
     
     if user_message in ("3"):
         return "Placeholder 3"
