6430794783e832e97530cf634d1c7a3384313e83
Andres F Parra <108076867+AndresfelipePa@users.noreply.github.com>
2023-09-07 15:58:23 -0500

    Update responses.py

--- responses.py
+++ responses.py
@@ -6,8 +6,8 @@ def sample_responses(input_text):
     if user_message in ("hola"):
         return "Hola soy un chatbot de autorrespuesta, escoge la pregunta que quieras hacer \n\n1)Pregunta 1 \n2)Pregunta 2 \n3)Pregunta 3 \n4)Pregunta 4 \n5)Pregunta 5 \n6)Pregunta 6 \n7)Contactanos"
     
-    if user_message in ("1"):
-        return "Placeholder 1"
+    if user_message in ("1) ¿Cuándo inician las inscripciones?"):
+        return "Cordial saludo La Universidad convocará semestralmente la inscripción de aspirantes para ingresar a sus diferentes programas académicos, mediante aviso en un periódico de circulación nacional y regional, en las carteleras de la Institución y en otros medios masivos de comunicación, anunciando los requisitos exigidos y las fechas establecidas para tal efecto."
     
     if user_message in ("2"):
         return "Placeholder 2"
