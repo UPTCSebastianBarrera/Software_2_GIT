49cd76787a12a158a253c6b074598cce75a61aba
UPTCSebastianBarrera <joan.barrera01@uptc.edu.co>
2023-09-21 16:44:43 -0500

    controler javascrip guardar preguntas y respuestas

---
+++ Interface/page1script.js
@@ -0,0 +1,18 @@
+// scrip code for chatbot
+
+const AddBtn = document.querySelector(".Add");
+const ClearBtn = document.querySelector(".Clear");
+
+const AddQInput = document.querySelector(".AddQInput");
+const AddAInput = document.querySelector(".AddAInput");
+
+const SearchQInput = document.querySelector(".SearchQInput");
+
+const QAs = document.querySelector(".QA");
+
+const Addq =()=>{
+
+}
+
+
+AddBtn addEventListener("click", AddQ)
