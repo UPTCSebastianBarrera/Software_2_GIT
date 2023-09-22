86e7444cc4d818c222576a748f717c36e1079506
UPTCSebastianBarrera <joan.barrera01@uptc.edu.co>
2023-09-22 14:53:31 -0500

    script añadido a page1.html

--- Interface/page1script.js
+++ Interface/page1script.js
@@ -8,11 +8,37 @@ const AddAInput = document.querySelector(".AddAInput");
 
 const SearchQInput = document.querySelector(".SearchQInput");
 
-const QAs = document.querySelector(".QA");
+const QAs = document.querySelector(".QAs");
 
-const Addq =()=>{
+const AddQ =()=>{
 
+  const AddQInputContent = AddQInput.value;
+  // <div class="QA border-2 border border-warning rounded d-flex justify-content-between">
+  //   <p class="TaskContent p-2 text-justify">sdasfasf</p> 
+  //   <span class="bg-dark border rounded text-white py-3 px-2">X</span>
+  // </div>
+
+  const QA = document.createElement("div");
+  QA.classList.add("QA","border-2","border","border-warning","rounded","d-flex","justify-content-between");
+
+  const paragraph = document.createElement("p");
+  paragraph.classList.add("QAContent","p-2","text-justify");
+  paragraph.textContent = AddQInputContent;
+
+  QA.appendChild(paragraph);
+
+  const spna = document.createElement("span");
+  spna.classList.add("bg-dark","border","rounded","text-white","py-3","px-2")
+  QA.appendChild(spna);
+
+  const a = document.createElement("a");
+  a.classList.add("text-decoration-none");
+  a.textContent="X";
+  spna.appendChild(a);
+
+  QAs.appendChild(QA);
+  
 }
 
 
-AddBtn addEventListener("click", AddQ)
+AddBtn.addEventListener("click", AddQ);
