ae0b6bf9584ba95cd6a44cad180a82d46fb177ed
Daniel Carrillo <daniel.carrillo01@uptc.edu.co>
2023-09-25 09:52:51 -0500

    Se configura index.html y la lógica con javascript para login a la app

--- Interface/js/login.js
+++ Interface/js/login.js
@@ -1,19 +1,20 @@
-document.addEventListener('DOMContentLoaded', function(){
-    // Validación de formulario
+document.getElementById("ingresarBtn").addEventListener("click", function() {
+    const usuarioInput = document.getElementById("usuarioInput").value;
+    const contrasenaInput = document.getElementById("contrasenaInput").value;
 
-
-    eventListeners();
+    fetch("/Software_2_GIT/Interface/assets/users.json") // Cargamos el archivo JSON
+        .then(response => response.json())
+        .then(data => {
+            const usuarios = data.usuarios;
+            const usuarioValido = usuarios.find(user => user.mail === usuarioInput && user.password === contrasenaInput);
+            if (usuarioValido) {
+                alert("Inicio de sesión exitoso.");
+                // Aquí puedes redirigir al usuario a otra página si es necesario.
+            } else {
+                alert("Credenciales incorrectas. Por favor, inténtalo de nuevo.");
+            }
+        })
+        .catch(error => {
+            console.error("Error al cargar el archivo JSON: " + error);
+        });
 });
-
-function eventListeners(){
-    passwordValidation();
-}
-
-function passwordValidation(){
-    const button = document.querySelector('.btn');
-    button.addEventListener('click', (e)=>{
-        e.preventDefault();
-        console.log('Oprimiendo boton');
-    })
-
-}
